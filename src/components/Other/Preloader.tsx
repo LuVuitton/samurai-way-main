f7bcdc18d8fcbc3e12c73b2bbbf229c3930aa027
FelixT <felixtekhada@gmail.com>
2023-04-29 00:31:38 +0200

    Updates

--- src/components/Other/Preloader.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Preloader = ()=> {
-    return  <div>Loading...</div>
-}
--- src/customHooks/useCustomDispatch.ts
+++
@@ -1,7 +0,0 @@
-
-
-import {ThunkDispatch} from "redux-thunk";
-import {RootStateType} from "../Redux/Store";
-import {AnyAction} from "redux";
-
-export type AppDispatchType = ThunkDispatch<RootStateType, any, AnyAction>
